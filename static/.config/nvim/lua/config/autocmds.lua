e66e82e8a30da7bbc8be462bf19cb98b5654656c
Izel Nakri <contact@izelnakri.com>
2024-05-26 18:49:32 +0200

    nvim get selected text

--- static/.config/nvim/lua/config/autocmds.lua
+++ static/.config/nvim/lua/config/autocmds.lua
@@ -2,18 +2,26 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+function Custom:getSelection()
+  vim.api.nvim_feedkeys("ygv", "n", false)
+  vim.schedule(function()
+    local text = vim.fn.getreg('"')
+    LazyVim.notify(text)
+  end)
+  -- NOTE: also there can be a version without yank/register, just mark
+end
 vim.cmd("command! -nargs=0 Name execute ':!echo % | wl-copy'")
 vim.cmd("command! RmSwp execute '!rm /var/tmp/*.swp'")
 vim.cmd("command! Errors execute ':Telescope notify'")
 
 -- NOTE: This makes LSP activated for the buffer:
 local function augroup(name)
-  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+  return vim.api.nvim_create_augroup("my_custom_" .. name, { clear = true })
 end
 
 vim.api.nvim_create_autocmd("BufEnter", {
   group = augroup("activate_lsp"),
-  callback = function(event)
+  callback = function() -- NOTE: param event
     vim.cmd([[filetype detect]])
   end,
 })
